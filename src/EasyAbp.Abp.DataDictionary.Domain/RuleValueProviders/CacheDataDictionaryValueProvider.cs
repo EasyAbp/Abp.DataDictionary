9f2c3098dfafc5c79f5d840ed55039ffbbfa3508
GameBelial <243387971@qq.com>
2020-09-22 21:00:34 +0800

    Improve localized text prompts.

--- src/EasyAbp.Abp.DataDictionary.Domain/RuleValueProviders/CacheDataDictionaryValueProvider.cs
+++ src/EasyAbp.Abp.DataDictionary.Domain/RuleValueProviders/CacheDataDictionaryValueProvider.cs
@@ -1,8 +1,11 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using EasyAbp.Abp.DataDictionary.Localization;
+using Microsoft.Extensions.Localization;
 using Volo.Abp.Caching;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Domain.Entities;
+using Volo.Abp.Localization;
 
 namespace EasyAbp.Abp.DataDictionary.RuleValueProviders
 {
@@ -10,12 +13,15 @@ namespace EasyAbp.Abp.DataDictionary.RuleValueProviders
     {
         protected readonly IDistributedCache<DataDictionaryItemInfo> DistributedCache;
         protected readonly IDataDictionaryRepository DataDictionaryRepository;
+        protected readonly IStringLocalizer<DataDictionaryResource> StringLocalizer;
 
         public CacheDataDictionaryValueProvider(IDistributedCache<DataDictionaryItemInfo> distributedCache,
-            IDataDictionaryRepository dataDictionaryRepository)
+            IDataDictionaryRepository dataDictionaryRepository,
+            IStringLocalizer<DataDictionaryResource> stringLocalizer)
         {
             DistributedCache = distributedCache;
             DataDictionaryRepository = dataDictionaryRepository;
+            StringLocalizer = stringLocalizer;
         }
 
         public virtual async Task<string> GetValueAsync(string dictCode, string itemCode)
@@ -26,8 +32,7 @@ namespace EasyAbp.Abp.DataDictionary.RuleValueProviders
                 var item = dict.Items.FirstOrDefault(i => i.Code == itemCode);
                 if (item == null)
                 {
-                    // TODO: Should be localized.
-                    throw new EntityNotFoundException("数据字典不存在，无法获取对应的值。");
+                    throw new EntityNotFoundException(StringLocalizer["DataDictionary:CacheItemNotFound"]);
                 }
 
                 return new DataDictionaryItemInfo
