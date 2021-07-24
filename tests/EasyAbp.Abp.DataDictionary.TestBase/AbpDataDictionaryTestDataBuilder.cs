5d2a5e9bf81071839d6e68199ccc2725dd16f4e1
real-zony <zony@myzony.com>
2021-07-24 12:09:18 +0800

    fix: repaired the compilation error problem.

--- tests/EasyAbp.Abp.DataDictionary.TestBase/AbpDataDictionaryTestDataBuilder.cs
+++ tests/EasyAbp.Abp.DataDictionary.TestBase/AbpDataDictionaryTestDataBuilder.cs
@@ -27,23 +27,23 @@ namespace EasyAbp.Abp.DataDictionary
             await _dataDictionaryRepository.InsertAsync(new DataDictionary(_testData.DataDictionary1, null, "MZ", "民族", null,
                 new List<DataDictionaryItem>
                 {
-                    new DataDictionaryItem(_testData.DataDictionary1, null, "1", "汉族", null),
-                    new DataDictionaryItem(_testData.DataDictionary1, null, "2", "羌族", null)
+                    new DataDictionaryItem(_testData.DataDictionary1, null, "1", "汉族", null, true),
+                    new DataDictionaryItem(_testData.DataDictionary1, null, "2", "羌族", null, true)
                 }));
 
             await _dataDictionaryRepository.InsertAsync(new DataDictionary(_testData.DataDictionary2, null, "DJ", "等级", null,
                 new List<DataDictionaryItem>
                 {
-                    new DataDictionaryItem(_testData.DataDictionary2, null, "1", "一级", null),
-                    new DataDictionaryItem(_testData.DataDictionary2, null, "2", "二级", null)
+                    new DataDictionaryItem(_testData.DataDictionary2, null, "1", "一级", null, true),
+                    new DataDictionaryItem(_testData.DataDictionary2, null, "2", "二级", null, true)
                 }));
 
             await _dataDictionaryRepository.InsertAsync(new DataDictionary(_testData.DataDictionary3, null, "XL", "学历", null,
                 new List<DataDictionaryItem>
                 {
-                    new DataDictionaryItem(_testData.DataDictionary3, null, "1", "高中", null),
-                    new DataDictionaryItem(_testData.DataDictionary3, null, "2", "大专", null),
-                    new DataDictionaryItem(_testData.DataDictionary3, null, "3", "本科", null),
+                    new DataDictionaryItem(_testData.DataDictionary3, null, "1", "高中", null, true),
+                    new DataDictionaryItem(_testData.DataDictionary3, null, "2", "大专", null, true),
+                    new DataDictionaryItem(_testData.DataDictionary3, null, "3", "本科", null, true)
                 }));
         }
     }
