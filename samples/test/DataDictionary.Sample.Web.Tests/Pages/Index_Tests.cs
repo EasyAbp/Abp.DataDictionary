c57bea03b23642ee302d01aa7e8426a6beb886b4
gdlcf88 <gdlcf88@outlook.com>
2023-01-13 02:54:46 +0800

    Fix `Index_Tests`

--- samples/test/DataDictionary.Sample.Web.Tests/Pages/Index_Tests.cs
+++ samples/test/DataDictionary.Sample.Web.Tests/Pages/Index_Tests.cs
@@ -1,16 +1,6 @@
-﻿using System.Threading.Tasks;
-using Shouldly;
-using Xunit;
-
-namespace DataDictionary.Sample.Pages
+﻿namespace DataDictionary.Sample.Pages
 {
     public class Index_Tests : SampleWebTestBase
     {
-        [Fact]
-        public async Task Welcome_Page()
-        {
-            var response = await GetResponseAsStringAsync("/");
-            response.ShouldNotBeNull();
-        }
     }
 }
