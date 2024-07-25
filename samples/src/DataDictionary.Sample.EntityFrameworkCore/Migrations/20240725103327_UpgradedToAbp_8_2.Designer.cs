fd96b75e2d929190364ea78d6ef9e8f6116c1607
gdlcf88 <gdlcf88@outlook.com>
2024-07-25 18:34:20 +0800

    Fix sample

--- samples/src/DataDictionary.Sample.EntityFrameworkCore/Migrations/20240725103327_UpgradedToAbp_8_2.Designer.cs
+++ samples/src/DataDictionary.Sample.EntityFrameworkCore/Migrations/20240725103327_UpgradedToAbp_8_2.Designer.cs
@@ -4,6 +4,7 @@ using DataDictionary.Sample.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace DataDictionary.Sample.Migrations
 {
     [DbContext(typeof(SampleDbContext))]
-    partial class SampleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240725103327_UpgradedToAbp_8_2")]
+    partial class UpgradedToAbp_8_2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
