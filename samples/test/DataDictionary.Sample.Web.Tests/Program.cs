4be875e4c9e0863f90e9a8174d64459e99277b7d
gdlcf88 <gdlcf88@outlook.com>
2024-12-29 18:45:48 +0800

    Fix the test project builder configurations

--- samples/test/DataDictionary.Sample.Web.Tests/Program.cs
+++ samples/test/DataDictionary.Sample.Web.Tests/Program.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Builder;
 using Volo.Abp.AspNetCore.TestBase;
 
 var builder = WebApplication.CreateBuilder();
-await builder.RunAbpModuleAsync<SampleWebTestModule>();
+
+builder.Environment.ContentRootPath = GetWebProjectContentRootPathHelper.Get("DataDictionary.Sample.Web.csproj");
+await builder.RunAbpModuleAsync<SampleWebTestModule>(applicationName: "DataDictionary.Sample.Web" );
 
 public partial class Program
 {
